b3bbfde07fe464e07b4980fbc5f7dd1445aadd20
vgmariucci <vgmariucci@hotmail.com>
2026-01-27 15:57:49 -0300

    Inclusion of 01_baby_python directory and 01_hello_world.py

---
+++ 01_baby_python/src/01_hello_world.py
@@ -0,0 +1,13 @@
+################################################################
+# This file is part of the Hello World project.                #
+################################################################
+
+def hello_world():
+    """
+    Prints 'Hello, World!' to the console.
+    """
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    hello_world()
---
+++ 01_baby_python/src/02_fizzbuzz.py
@@ -0,0 +1,27 @@
+'''
+This Python script is intended to implement the FizzBuzz problem.
+The FizzBuzz problem is a common programming challenge where you print numbers from 1 to 100.
+'''
+
+def FizzBuzz():
+    '''
+    - For multiples of three, print "Fizz" instead of the number.
+    - For multiples of five, print "Buzz" instead of the number.
+    - For numbers which are multiples of both three and five, print "FizzBuzz".
+    '''
+    for i in range(1,100):
+        
+        if(i % 3 == 0 and not(i % 5 == 0)):
+            print(f"{i} Fizz")
+
+        if(i % 5 == 0 and not(i % 3 == 0)):
+            print(f"{i} Buzz")
+
+        if(i % 15 == 0):
+            print(f"{i} FizzBuzz")
+        
+        else:
+            print(i)
+    
+# Call the FizzBuzz function to execute the logic
+FizzBuzz()
