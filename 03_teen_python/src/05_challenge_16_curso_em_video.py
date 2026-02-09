6a7df6b54ab5c724a660688de5917f52dcfbc2dd
vgmariucci <vgmariucci@hotmail.com>
2026-02-08 22:36:58 -0300

    Typo correction in 05_challenge_16_curso_em_video.py file

--- 03_teen_python/src/05_challenge_16_curso_em_video.py
+++ 03_teen_python/src/05_challenge_16_curso_em_video.py
@@ -1,9 +1,9 @@
 '''
-A class to represent a colaborator of a comphany
+A class to represent a collaborator of a comphany
 '''
-class Colaborator:
+class Collaborator:
     '''
-    Creates the colaborator with basic attributes and methods
+    Creates the collaborator with basic attributes and methods
     '''
     def __init__(self, name, department, position, company = "CompCorp"):
 
@@ -16,9 +16,9 @@ class Colaborator:
     
         return print(f"Hi, my name is {self.name}. I'm the {self.position} at the {self.department} department at {self.company}")
 
-c1 = Colaborator(name = "Maria", department = "Marketing", position = "Analyst")
+c1 = Collaborator(name = "Maria", department = "Marketing", position = "Analyst")
 
-c2 = Colaborator(name = "John", department = "IT", position = "Support Technician")
+c2 = Collaborator(name = "John", department = "IT", position = "Support Technician")
 
 c1.PresentYourSelf()
 c2.PresentYourSelf()
